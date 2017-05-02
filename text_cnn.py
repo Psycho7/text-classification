c7a37c1a29b7efb61f269fdfca23db1bdcb0e73f
Chujie Zeng <chujie.zeng@gmail.com>
2017-05-02 17:18:03 +0800

    Add text_cnn.py

---
+++ text_cnn.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import numpy as np
+
+class TextCNN(object):
+    def __init__(
+            self, sequence_length, num_classes, vocab_size,
+            embedding_size, filter_sizes, num_filters, l2_reg_lambda):
+
+        # Placeholders
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, sequence_length], name="input_y")
+        self.dropout_prob = tf.placeholder(tf.float32, name="dropout_prob")
+
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer
+        with tf.device("/cpu:0"), tf.name_scope("embedding"):
+            self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0, name="W"))
+            self.embedded = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedded_expanded = tf.expand_dims(self.embedded, -1)  #在最后加一维 (x, y) -> (x, y, 0)
+
+        # Conv Layer & Maxpool Layer
+        pooled_output = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Conv Layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1, name="W"))
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv = tf.nn.conv2d(
+                    self.embedded_expanded,
+                    W,
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",  # 卷积核不计算边界（不补0）
+                    name="conv")
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="ReLU")
+
+                pooled = tf.nn.max_pool(
+                    h,
+                    ksize=[1, sequence_length, embedding_size - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name="pool")    # 对每个卷积核的多个结果取MAX，每个卷积核仅对应一个结果
+                pooled_output.append(pooled)
+
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_output, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])  # reshape -> 1 * num_filters_total
+
+        # Dropout layer
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
